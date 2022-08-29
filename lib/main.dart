7eb1c923db7b9d862c5edcce29252950319d6912
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-29 10:49:08 -0300

    Adding a modalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import './models/transaction.dart';
 import 'package:flutter/material.dart';
 
-import './views/user_transactions.dart';
+import './views/transaction_list.dart';
+import './views/new_transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,29 +29,86 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Mesa nova',
+      amount: 239,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Mousepad gamer',
+      amount: 35,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    var random = Random.secure();
+
+    var valueId = utf8.encode(random.nextInt(1000000).toString());
+
+    final newTx = Transaction(
+        id: sha256.convert(valueId).toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas pessoais'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  width: double.infinity,
-                  child: const Card(
-                    child: Text('CHART!'),
-                    color: Colors.blue,
-                    elevation: 5,
-                  ),
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: const Card(
+                  child: Text('CHART!'),
+                  color: Colors.blue,
+                  elevation: 5,
                 ),
-                UserTransactions(),
-              ]),
-        ));
+              ),
+              TransactionList(_userTransactions),
+            ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _startAddNewTransaction(context),
+          child: Icon(Icons.add)),
+    );
   }
 }
