65590d060dbb3f45360cc4aa06dfbe25066c0cce
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-29 10:03:50 -0300

    Adding ListView builder

--- lib/views/transaction_list.dart
+++ lib/views/transaction_list.dart
@@ -3,49 +3,51 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionList extends StatelessWidget { 
+class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'R\$ ${tx.amount}',
-                  style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.purple,
-                      fontWeight: FontWeight.bold),
-                )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+        height: 300,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+                child: Row(
               children: [
-                Text(
-                  tx.title as String,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  DateFormat('dd/MM/yyyy').format(tx.date!),
-                  style: const TextStyle(color: Colors.grey),
+                Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${transactions[index].amount}',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold),
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].title as String,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      DateFormat('dd/MM/yyyy').format(transactions[index].date!),
+                      style: const TextStyle(color: Colors.grey),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            ));
+          },
+          itemCount: transactions.length,
         ));
-      }).toList(),
-    );
   }
 }
