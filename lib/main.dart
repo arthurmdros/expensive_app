745f0bad7e9116dde42d4c0d8578a11b9ed4b176
arthurmdros <arthurmedeiros2005@gmail.com>
2022-09-07 15:13:08 -0300

    Adicionando altura dinamicamente para o container de listagens

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ import './views/new_transaction.dart';
 import './views/transaction_list.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+  //WidgetsFlutterBinding.ensureInitialized();
+  //SystemChrome.setPreferredOrientations([
+    //DeviceOrientation.portraitUp,
+    //DeviceOrientation.portraitDown,
+  //]);
   runApp(MyApp());
 }
 
@@ -74,6 +74,8 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // )
   ];
+
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((element) {
@@ -138,11 +140,22 @@ class _MyHomePageState extends State<MyHomePage> {
             // mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Exibir gráfico'),
+                  Switch(value: _showChart, onChanged: (val) {
+                    setState(() {
+                      _showChart = val;
+                    });
+                  })
+                ],
+              ),
+              _showChart ? Container(
                   height: (MediaQuery.of(context).size.height -
                           appBar.preferredSize.height - MediaQuery.of(context).padding.top) *
-                      0.3,
-                  child: Chart(_recentTransactions)),
+                      0.7,
+                  child: Chart(_recentTransactions)) :
               Container(
                   height: (MediaQuery.of(context).size.height -
                           appBar.preferredSize.height - MediaQuery.of(context).padding.top) *
